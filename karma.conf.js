33a39a1add29bbca7643ce47e519efee47d1b452
Wildhoney <adam.timberlake@gmail.com>
2015-01-21 11:07:26 +0000

    Setup SauceLabs

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,14 @@
 module.exports = function(config) {
 
+    var customLaunchers = {
+        sl_chrome: {
+            base: 'SauceLabs',
+            browserName: 'Chrome',
+            platform: 'OS X 10.9',
+            version: '39'
+        }
+    };
+
     config.set({
 
         frameworks: ['jasmine'],
@@ -7,12 +16,19 @@ module.exports = function(config) {
             'module/UploadButton.js',
             'tests/spec.js'
         ],
-        reporters: ['progress'],
+        sauceLabs: {
+            testName: 'UploadButton',
+            connectOptions: {
+                verbose: true
+            }
+        },
+        customLaunchers: customLaunchers,
+        browsers: Object.keys(customLaunchers),
+        reporters: ['progress', 'saucelabs'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: false,
-        browsers: ['Chrome', 'ChromeCanary'],
         singleRun: true
 
     });
