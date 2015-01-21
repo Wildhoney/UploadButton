3bc4417edb73caadadc09ce7e0aa4899c6e1ef30
Wildhoney <adam.timberlake@gmail.com>
2015-01-21 12:18:37 +0000

    Added 'startConnect'

--- karma.conf.js
+++ karma.conf.js
@@ -18,6 +18,7 @@ module.exports = function(config) {
         ],
         sauceLabs: {
             testName: 'UploadButton',
+            startConnect: false,
             connectOptions: {
                 verbose: true
             }
